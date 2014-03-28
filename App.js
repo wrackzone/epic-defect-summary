d4196f453c6b7fdcfdb72ca89acc8827958b4897
Barry Mullan <mullan@gmail.com>
2014-03-28 13:09:09 -0400

    first version

---
+++ App.js
@@ -0,0 +1,110 @@
+var app = null;
+
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    // items:{ html:'<a href="https://help.rallydev.com/apps/2.0rc2/doc/">App SDK 2.0rc2 Docs</a>'},
+    launch: function() {
+        app = this;
+        console.log("Hello World!");
+        this.addFeatureGrid();
+    },
+
+    defectColumn : {  
+        text: "Defects", width:100, 
+        renderer : function(value, metaData, record, rowIdx, colIdx, store, view) {
+            var defects = record.get("Defects");
+            if (defects && defects.length > 0) {
+                var states = _.countBy(defects, function(d) { 
+                    return d.get("State")!= "Closed" ? "Open" : "Closed";
+                });
+                states.Open = states.Open !== undefined ? states.Open : 0;
+                    // states.Open = 0 
+                states.length = defects.length;
+                var tpl = Ext.create('Ext.Template', "{Open}/{length}", { compiled : true } );
+                return tpl.apply(states);
+            } else
+                return "";
+        }
+    },
+    
+    addFeatureGrid : function() {
+        var viewport = Ext.create('Ext.Viewport');
+        Rally.data.ModelFactory.getModel({
+         // type: 'PortfolioItem/Feature',
+         type: 'HierarchicalRequirement',
+         success: function(userStoryModel) {
+
+         	var grid = Ext.create('Rally.ui.grid.Grid', {
+                 // xtype: 'rallygrid',
+                 model: userStoryModel,
+
+                 listeners : {
+                    load : function(items) {
+                        console.log("load",items.data.items);
+                        var features = items.data.items;
+                        async.map(features,app.getSnapshots, function(err,results) {
+                            _.each( features, function(feature,i){
+                                feature.set("Defects",results[i]);
+                            })
+                        });
+                    }
+                 },
+                 columnCfgs: [
+                     'FormattedID',
+                     'Name',
+                     'Owner',
+                     app.defectColumn
+                 ]
+            });
+
+            viewport.add(grid);
+
+            var filterConfig = {
+	            property:'Parent',
+	            operator: '=',
+	            value: null
+			};
+            
+            grid.filter(filterConfig, true, true);
+
+
+         }
+        });
+    },
+    
+    getSnapshots : function(record, callback) {
+
+        var that = this;
+        var fetch = ['ObjectID','_UnformattedID','State','Priority','Severity','_ItemHierarchy','_TypeHierarchy'];
+        var hydrate = ['_TypeHierarchy','State','Priority','Severity'];
+        
+        var find = {
+                '_TypeHierarchy' : { "$in" : ["Defect"]} ,
+                '_ProjectHierarchy' : { "$in": app.getContext().getProject().ObjectID },
+                '__At' : 'current',
+                "_ItemHierarchy" : { "$in" : record.get("ObjectID")  }
+        };
+
+        var storeConfig = {
+            find : find,
+            autoLoad : true,
+            pageSize:1000,
+            limit: 'Infinity',
+            fetch: fetch,
+            hydrate: hydrate,
+            listeners : {
+                scope : this,
+                load: function(store, snapshots, success) {
+                    // console.log("success",success);
+                    // console.log("completed snapshots:", snapshots.length);
+                    callback(null,snapshots);
+                }
+            }
+        };
+
+        var snapshotStore = Ext.create('Rally.data.lookback.SnapshotStore', storeConfig);
+
+    }
+    
+});
